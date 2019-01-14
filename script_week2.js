81c0ec2fce7e8dca3e2af2f3f0d8661dfe376a07
SimarjitKaur10 <43039651+SimarjitKaur10@users.noreply.github.com>
2019-01-14 16:53:47 -0500

    WEEK-2 CLASS HAND IN WORK

---
+++ script_week2.js
@@ -0,0 +1,16 @@
+// Name:Simarjit Kaur
+// Date: January 14,2019
+// Student: C0741324
+// Course: CSD 2214 Section 6
+
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('#clock');
+const daysspan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
+
+
+//console.log(endDate.value);
